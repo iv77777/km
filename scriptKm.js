f343cdc64a12ba20196a3484c1b31c350079f232
iv77777 <70826113+iv77777@users.noreply.github.com>
2022-01-06 16:51:38 +0200

    Add files via upload

--- scriptKm.js
+++ scriptKm.js
@@ -7,14 +7,32 @@ var rounded = function (number) {
   return +number.toFixed(2);
 }
 
+let valueKm;
+
+
+
 buttonKm.addEventListener('click', function(){
   const inputRefuelingKm = document.querySelector('#inputRefuelingKm');
   const inputKm = document.querySelector('#inputKm');
 
   // вищитуем росход газа на 100 км.
   if (inputRefuelingKm.value > 0 && inputKm.value > 0){
-    resultKm.innerHTML = rounded(inputRefuelingKm.value / (inputKm.value / 100));
+    valueKm = rounded(inputRefuelingKm.value / (inputKm.value / 100));
+    resultKm.innerHTML = valueKm;
   }
 
+
+
+
+  // ********************<< localStorage >>*****************************************************
+  let usObjectKm = [{ valueKm: '' }];
+
+  usObjectKm[0].valueKm = valueKm;
+
+  // переобразовуем масив valueKmObject в строку и записываем в valueKmObjectText
+  let valueKmObjectText = JSON.stringify(usObjectKm);
+  // добавляем valueKmObjectText в localStorage
+  localStorage.setItem("usObjectKm", valueKmObjectText);
+// ********************<< // localStorage >>*****************************************************
 });
 
